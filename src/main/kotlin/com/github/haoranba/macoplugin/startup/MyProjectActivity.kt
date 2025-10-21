38cc003213632c217c1b283d6fae6ab71805a6a8
GitHub Action <action@github.com>
2025-10-21 03:01:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/haoranba/macoplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/haoranba/macoplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.haoranba.macoplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
